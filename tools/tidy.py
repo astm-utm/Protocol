0846c9ecc2db3a20eac904c50883b2b817b469c6
Benjamin Pelletier <pelletierb@wing.com>
2021-09-27 14:39:54 -0700

    Format whitespace consistently

--- tools/tidy.py
+++ tools/tidy.py
@@ -1,5 +1,24 @@
 from ruamel.yaml import YAML
 
+with open('../utm.yaml', 'r') as f:
+    lines = f.readlines()
+i = 0
+while i < len(lines):
+    line = lines[i]
+    colon = line.find(':')
+    is_ref = line.find('$ref:')
+    if len(line) > 80 and colon > 0 and is_ref == -1:
+        indent = len(line) - len(line.lstrip())
+        line1 = line[0:colon+1] + ' >-' + '\n'
+        line2 = (' ' * (indent + 2)) + line[colon+1:]
+        lines = lines[0:i] + [line1, line2] + lines[i+1:]
+        i += 2
+    else:
+        i += 1
+
+with open('../utm.yaml', 'w') as f:
+    f.writelines(lines)
+
 yaml = YAML()
 with open('../utm.yaml', 'r') as f:
     api = yaml.load(f)
